640c1609d0ed34c3cd4699ffdcc16b1aabcada17
Maitri Modi <maitrimodi19@gmail.com>
2019-12-03 19:28:35 -0500

    Layout created

---
+++ Pages_Database/Home.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Pages_Database.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="body" runat="server">
+    <div>
+        <img src="background_search.jpg" alt="background image of home page" class="home_image"/>
+    </div>
+</asp:Content>
---
+++ Pages_Database/PAGESDB.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace Pages_Database
+{
+    public class PAGESDB
+    {
+        //things that describe a database
+        // - A connection (url, port)
+        // - A username
+        // - A password
+        // - A database
+
+        //do not expose these pieces of information!
+        //how do I know these?
+        //boot up MAMP, open start page
+        //info is right below the PHPmyAdmin link
+        //THIS INFO IS FOR A PUBLIC DATABASE THAT I CREATED, IT HAS READ-ONLY ACCESS
+        private static string User { get { return "root"; } }
+        private static string Password { get { return " "; } }
+        private static string Database { get { return "pages_db"; } }
+        private static string Server { get { return "localhost"; } }
+        private static string Port { get { return "3306"; } }
+
+        //ConnectionString is something that we use to connect to a database
+        private static string ConnectionString {
+            get {
+                return "server = "+Server
+                    +"; user = "+User
+                    +"; database = "+Database
+                    +"; port = "+Port
+                    +"; password = "+Password;
+            }
+        }
+
+        //returns a result set
+        //is a list dictionaries
+        //a dictionary is like a list but with Key:Value pairs
+        //they are also known as "associative arrays"
+        public List<Dictionary<String,String>> List_Query(string query)
+        {
+            MySqlConnection Connect = new MySqlConnection(ConnectionString);
+
+            // INPUT -> (string) SELECT QUERY 
+            // e.g. "select * from students";
+            // OUTPUT -> (List<Dictionary<String,String>>) RESULT SET 
+            // e.g. 
+            //[
+            //  {"STUDENTFNAME":"SARAH","STUDENTLNAME":"Valdez","STUDENTNUMBER":"N1678","ENROLMENTDATE":"2018-06-18"},
+            //  {"STUDENTFNAME":"Jennifer","STUDENTLNAME":"FAULKNER","STUDENTNUMBER":"N1679","ENROLMENTDATE":"2018-08-02"},
+            //  {"STUDENTFNAME":"Austin","STUDENTLNAME":"Simon","STUDENTNUMBER":"N1682","ENROLMENTDATE":"2018-06-14"},
+            //  ...
+            //] 
+            List<Dictionary<String, String>> ResultSet = new List<Dictionary<String, String>>();
+
+            // try{} catch{} will attempt to do everything inside try{}
+            // if an error happens inside try{}, then catch{} will execute instead.
+            // very useful for debugging without the whole program crashing!
+            // this can be easily abused and should be used sparingly.
+            try
+            {
+                Debug.WriteLine("Connection Initialized...");
+                Debug.WriteLine("Attempting to execute query "+query);
+                //open the db connection
+                Connect.Open();
+                //give the connection a query
+                MySqlCommand cmd = new MySqlCommand(query, Connect);
+                //grab the result set
+                MySqlDataReader resultset = cmd.ExecuteReader();
+
+                
+                //for every row in the result set
+                while (resultset.Read())
+                {
+                    Dictionary<String,String> Row = new Dictionary<String, String>();
+                    //for every column in the row
+                    for(int i = 0; i < resultset.FieldCount; i++)
+                    {
+                        Row.Add(resultset.GetName(i), resultset.GetString(i));
+                        
+                    }
+                    
+                    ResultSet.Add(Row);
+                }//end row
+                resultset.Close();
+
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Something went wrong in the List_Query method!");
+                Debug.WriteLine(ex.ToString());
+               
+            }
+
+            Connect.Close();
+            Debug.WriteLine("Database Connection Terminated.");
+
+            return ResultSet;
+        }
+
+
+        //The objective of this method in the schooldb class is to find a particular student given an integer ID
+        //instead of returning a dictionary we will return type "STUDENT" in our Student.cs class
+       
+
+    }
+}
